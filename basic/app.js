4d7ab867018d671039d80c86a6a7a7ba9c486b46
Arxgon <iqbalaf@students.undip.ac.id>
2021-11-05 09:34:09 +0700

    basic1

---
+++ basic/app.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+var num1 = 5;
+var num2 = 1;
+var result = add(num1, num2);
+console.log(result);
+// learn object
+var person = {
+    name: "Me",
+    age: 23
+};
+var animal = {
+    name: "cat",
+    species: {
+        first: "felix",
+        second: "felus"
+    }
+};
+console.log(person.name);
